f388e1b4a11116b7de7659e8f4f16d06c6044e2f
charliekaks <kaksunchained@gmail.com>
2020-05-08 15:16:48 +0300

    learned about component communication and added data to my list of movies.

--- src/app/movie-item/movie-item.component.ts
+++ src/app/movie-item/movie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-movie-item',
@@ -7,6 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MovieItemComponent implements OnInit {
   @Input() movies : any;
+  @Output() clickMe = new EventEmitter();
+
+  clickButton(){
+    this.clickMe.emit(this.movies.name);
+  }
   constructor() { }
 
   ngOnInit(): void {
